3002971e89cbf2fd2798e178d7978ff3b1633b8e
admin <admin@Mac.local>
2018-01-24 13:42:10 +0800

    ex1.py

---
+++ ex1.py
@@ -0,0 +1,22 @@
+print "hello world!"
+print "hello again"
+print "i like type this,"
+print "this is fun."
+print 'yay! printing.'
+print "i'd much rather you 'not'."
+print 'i "said" do not touch this.'
+
+#5
+my_name='Zed A.Shaw'
+my_age=35#not a lie
+my_height=74#inches
+my_weight=180#lbs
+my_eyes='blue'
+my_teeth = 'white'
+my_hair = 'brown'
+print ("let's talk about %s." % my_name) 
+print("he's %d inches tall.") 
+print("he's %d pounds heavy.") % my_weight
+print("actually that's not too heavy.")
+print("he's got %s eyes and %s hair." % (my_eyes, my_hair)) 
+print("his teeth are usually %s depending on the coffee.") % my_teeth
